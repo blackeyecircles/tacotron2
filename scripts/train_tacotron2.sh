ba05f6cc5e93cb08c4be7d366d85a75e9996140f
xinzheshen <xinzheshen@163.com>
2020-01-04 11:32:35 +0800

    update readme

--- scripts/train_tacotron2.sh
+++ scripts/train_tacotron2.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python train.py -o logs --init-lr 1e-3 --final-lr 1e-5 --epochs 200 -bs 32 --weight-decay 1e-6 --log-file nvlog.json --dataset-path /home/shenxz/dataset/training_data --training-anchor-dirs BZNSYP
+CUDA_VISIBLE_DEVICES=0 python train_tacotron2.py -o logs --init-lr 1e-3 --final-lr 1e-5 --epochs 200 -bs 32 --weight-decay 1e-6 --log-file nvlog.json --dataset-path /home/shenxz/dataset/training_data --training-anchor-dirs BZNSYP
